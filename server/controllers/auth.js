188467360c0f5659f207a3e6c7c9f2a6d395cecb
Selmen Rayen <selmanerayene95@gmail.com>
2023-06-15 01:38:51 +0100

    add login

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,7 +1,8 @@
-import { hashPassword } from "../helpers/auth.js";
+import { comparePassword, hashPassword } from "../helpers/auth.js";
 import User from "../models/user.js";
 import jwt from "jsonwebtoken";
 
+//REGISTER
 export const register = async (req, res) => {
   try {
     const { name, email, password } = req.body;
@@ -30,6 +31,46 @@ export const register = async (req, res) => {
     const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
       expiresIn: "1d",
     });
+    res.json({
+      user: {
+        name: user.name,
+        email: user.email,
+        role: user.role,
+        address: user.address,
+      },
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//LOGIN
+export const login = async (req, res) => {
+  try {
+    const {  email, password } = req.body;
+
+    if (!email) {
+      return res.json({ error: "Email is required" });
+    }
+    if (!password || password.length < 6) {
+      return res.json({ error: "Password must be at least 6 characters long" });
+    }
+
+    const user = await User.findOne({ email }); //email:email
+    if (!user) {
+      return res.json({ error: "User not found" });
+    }
+
+    const match = await comparePassword(password , user.password);
+    if (!match) {
+      res.json({error: 'Wrong Password'})
+    }
+
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+
     res.json({
       user: {
         name: user.name,
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { register } from "../controllers/auth.js";
+import { register , login } from "../controllers/auth.js";
 
 const router = express.Router();
 
 router.post("/register", register );
+router.post("/login", login );
 
 export default router;
