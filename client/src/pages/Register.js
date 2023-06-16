84168db01b7c6826f0b4cdc164a50a3c965852c1
Selmen Rayen <selmanerayene95@gmail.com>
2023-06-16 20:13:53 +0100

    create register page

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import Jumbotron from "../components/cards/Jumbotron";
 
 const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <div>
-<Jumbotron title="Register"  />
+      <Jumbotron title="Register" />
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6 offset-md-3">
+            <input
+              type="text"
+              className="form-control mb-4 p-2"
+              placeholder="Enter your name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              autoFocus
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
