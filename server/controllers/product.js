eafec218cdfb23336f6c639449aba22e03991bcb
Selmen Rayen <selmanerayene95@gmail.com>
2023-06-16 01:32:15 +0100

    crud product

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -1,42 +1,147 @@
 import slugify from "slugify";
 import Product from "../models/product.js";
-import fs from 'fs'
+import fs from "fs";
 
+//CREAT A PRODUCT:
 export const create = async (req, res) => {
-    // console.log('rayyyy req:',req.fields)
+  //req.fields & req.files ==>Thanks to package : express-formidable
   try {
     const { name, description, price, category, quantity, shipping } =
-    req.fields;
-  const { photo } = req.files;
+      req.fields;
+    const { photo } = req.files;
     //Validation :
-      // validation
-      switch (true) {
-        case !name.trim():
-          return res.json({ error: "Name is required" });
-        case !description.trim():
-          return res.json({ error: "Description is required" });
-        case !price.trim():
-          return res.json({ error: "Price is required" });
-        case !category.trim():
-          return res.json({ error: "Category is required" });
-        case !quantity.trim():
-          return res.json({ error: "Quantity is required" });
-        case !shipping.trim():
-          return res.json({ error: "Shipping is required" });
-        case photo && photo.size > 1000000:
-          return res.json({ error: "Image should be less than 1mb in size" });
-      }
-      //Create product 
-      const product=new Product({...req.fields,slug:slugify(name)})
-      if (photo) {
-        product.photo.data=fs.readFileSync(photo.path) 
-        product.photo.contentType=photo.type
-      }
-      await product.save();
-      res.json(product)
-
+    // validation
+    switch (true) {
+      case !name.trim():
+        return res.json({ error: "Name is required" });
+      case !description.trim():
+        return res.json({ error: "Description is required" });
+      case !price.trim():
+        return res.json({ error: "Price is required" });
+      case !category.trim():
+        return res.json({ error: "Category is required" });
+      case !quantity.trim():
+        return res.json({ error: "Quantity is required" });
+      case !shipping.trim():
+        return res.json({ error: "Shipping is required" });
+      case photo && photo.size > 1000000:
+        return res.json({ error: "Image should be less than 1mb in size" });
+    }
+    //Create product
+    const product = new Product({ ...req.fields, slug: slugify(name) });
+    if (photo) {
+      product.photo.data = fs.readFileSync(photo.path);
+      product.photo.contentType = photo.type;
+    }
+    await product.save();
+    res.json(product);
   } catch (error) {
     console.log(error);
     return res.status(400).json(error);
+  }
+};
+
+//GET PRODUCTS:
+export const list = async (req, res) => {
+  try {
+    const products = await Product.find()
+      .populate("category") //To show all category data (not only Id)
+      .select("-photo")
+      .limit(12)
+      .sort({ createdAt: -1 });
+    res.json(products);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json(error);
+  }
+};
+
+//GET A PRODUCT:
+export const read = async (req, res) => {
+  try {
+    const product = await Product.findOne({ slug: req.params.slug })
+      .select("-photo")
+      .populate("category");
+    res.json(product);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json(error);
+  }
+};
+
+//GET  PRODUCT PHOTO:
+export const photo = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.productId).select(
+      "photo"
+    );
+    if (product.photo.data) {
+      /* res.set:==> Useful when returning binary data such as images, as it ensures 
+      that the client knows how to interpret and display the received data correctly. */
+      res.set("Content-Type", product.photo.contentType);
+      return res.send(product.photo.data);
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json(error);
+  }
+};
+
+//DELETE A PRODUCT:
+export const remove = async (req, res) => {
+  try {
+    const product = await Product.findByIdAndDelete(
+      req.params.productId
+    ).select("-photo");
+    res.json(product);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json(error);
+  }
+};
+
+//UPDATE A PRODUCT:
+export const update = async (req, res) => {
+  try {
+    const { name, description, price, category, quantity, shipping } =
+      req.fields;
+    const { photo } = req.files;
+    //Validation :
+    // validation
+    switch (true) {
+      case !name.trim():
+        return res.json({ error: "Name is required" });
+      case !description.trim():
+        return res.json({ error: "Description is required" });
+      case !price.trim():
+        return res.json({ error: "Price is required" });
+      case !category.trim():
+        return res.json({ error: "Category is required" });
+      case !quantity.trim():
+        return res.json({ error: "Quantity is required" });
+      case !shipping.trim():
+        return res.json({ error: "Shipping is required" });
+      case photo && photo.size > 1000000:
+        return res.json({ error: "Image should be less than 1mb in size" });
+    }
+
+    //Updated Product
+    const product = await Product.findByIdAndUpdate(
+      req.params.productId,
+      {
+        ...req.fields,
+        slug: slugify(name),
+      },
+      { new: true }
+    );
+    if (photo) {
+      product.photo.data = fs.readFileSync(photo.path);
+      product.photo.contentType = photo.type;
+    }
+    await product.save();
+    res.json(product);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json(error.message);
   }
 };
--- server/routes/product.js
+++ server/routes/product.js
@@ -1,11 +1,23 @@
 import express from "express";
-import formidable from 'express-formidable'
+import formidable from "express-formidable";
 
 const router = express.Router();
 
 import { isAdmin, requireSignin } from "../middlewares/auth.js";
-import { create } from "../controllers/product.js";
-//formidable() middleware ==> to receive form data
-router.post('/product', requireSignin , isAdmin , formidable(), create)
+import {
+  create,
+  list,
+  read,
+  photo,
+  remove,
+  update,
+} from "../controllers/product.js";
+//formidable() middleware ==> to receive form data (and receive file (img) data)
+router.post("/product", requireSignin, isAdmin, formidable(), create);
+router.get("/product", list);
+router.get("/product/:slug", read);
+router.get("/product/photo/:productId", photo);
+router.delete("/product/:productId", requireSignin, isAdmin, remove);
+router.put("/product/:productId", requireSignin, isAdmin, formidable(), update);
 
-export default router
+export default router;
