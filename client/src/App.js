90d4660a3615cc18dbc5c92ae594c7032726474c
Selmen Rayen <selmanerayene95@gmail.com>
2023-06-16 14:10:27 +0100

    add bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
-import logo from "./logo.svg";
-import "./App.css";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 function App() {
   return (
-    <div>
-      <h1>Hello !</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
 export default App;
