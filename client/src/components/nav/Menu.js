f69009caea439c397de9d112e2b7e47fec2b4159
Selmen Rayen <selmanerayene95@gmail.com>
2023-06-18 01:15:50 +0100

    logout

--- client/src/components/nav/Menu.js
+++ client/src/components/nav/Menu.js
@@ -1,6 +1,17 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, Navigate, useNavigate } from "react-router-dom";
+import { useAuth } from "../../context/auth";
+
 const Menu = () => {
+  const navigate = useNavigate();
+  //hook
+  const [auth, setAuth] = useAuth();
+
+  const logout = () => {
+    setAuth({ ...auth, token: "", user: null });
+    localStorage.removeItem("auth");
+    navigate("/login");
+  };
   return (
     <>
       <ul className="nav d-flex justify-content-between shadow-sm mb-2">
@@ -9,16 +20,26 @@ const Menu = () => {
             HOME
           </NavLink>
         </li>
-        <li className="nav-item">
-          <NavLink className="nav-link" to="/login">
-            LOGIN
-          </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink className="nav-link" to="/register">
-            REGISTER
-          </NavLink>
-        </li>
+        {!auth?.user ? (
+          <>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/login">
+                LOGIN
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/register">
+                REGISTER
+              </NavLink>
+            </li>
+          </>
+        ) : (
+          <li className="nav-item pointer">
+            <a className="nav-link" onClick={logout}>
+              LOGOUT
+            </a>
+          </li>
+        )}
       </ul>
     </>
   );
