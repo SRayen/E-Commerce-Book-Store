63ecb2e8d50f0bbbac72b3e6171960fad47ee773
Selmen Rayen <selmanerayene95@gmail.com>
2023-07-02 23:09:11 +0100

    animate Jumbotron

--- client/src/components/cards/Jumbotron.js
+++ client/src/components/cards/Jumbotron.js
@@ -1,14 +1,20 @@
 import React from "react";
 
-const Jumbotron = ({ title, subtitle = "Welcome to Rayen's Online Bookstore: Unlock the World of Technology and Knowledge" }) => {
+const Jumbotron = ({
+  title,
+  subtitle = "Welcome to Rayen's Bookstore",
+}) => {
   return (
     <div
       className="container-fluid jumbotron"
       style={{ marginTop: "-8px", height: "200px" }}
     >
-      <div className="col text-center p-5">
+      <div className="col text-center p-4">
         <h1 className="fw-bold">{title}</h1>
-        <p className="lead">{subtitle}</p>
+        <div className="content">
+          <h2 >{subtitle}</h2>
+          <h2 >{subtitle}</h2>
+        </div>
       </div>
     </div>
   );
