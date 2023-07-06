d561a51285b65fecb70c547ff0f8af6e84ad9117
Selmen Rayen <selmanerayene95@gmail.com>
2023-07-06 03:00:32 +0100

    add loading home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,6 +3,7 @@ import Jumbotron from "../components/cards/Jumbotron";
 import axios from "axios";
 import moment from "moment";
 import ProductCard from "../components/cards/ProductCard";
+import loadIcon from "../images/loading.gif";
 
 const Home = () => {
   const [products, setProducts] = useState([]);
@@ -30,11 +31,14 @@ const Home = () => {
   };
 
   const loadProducts = async () => {
+    setLoading(true);
     try {
       const { data } = await axios.get(`/list-products/${page}`);
       setProducts(data);
+      setLoading(false);
     } catch (error) {
       console.log(error);
+      setLoading(false);
     }
   };
 
@@ -62,25 +66,34 @@ const Home = () => {
             New Arrivals
           </h2>
 
-          <div className="row">
-            {products?.map((p) => (
-              <div className="col-md-6" key={p._id}>
-                <ProductCard p={p} />
-              </div>
-            ))}
-          </div>
+          {loading ? (
+            <img src={loadIcon} alt="loading" />
+          ) : (
+            <div className="row">
+              {products?.map((p) => (
+                <div className="col-md-6" key={p._id}>
+                  <ProductCard p={p} />
+                </div>
+              ))}
+            </div>
+          )}
         </div>
         <div className="col-md-6">
           <h2 className="p-3 mt-2 mb-2 h4 bg-dark text-center text-danger rounded-5">
             Best Sellers
           </h2>
-          <div className="row">
-            {sortedBySold?.map((p) => (
-              <div className="col-md-6" key={p._id}>
-                <ProductCard p={p} />
-              </div>
-            ))}
-          </div>
+
+          {loading ? (
+            <img src={loadIcon} alt="loading" />
+          ) : (
+            <div className="row">
+              {sortedBySold?.map((p) => (
+                <div className="col-md-6" key={p._id}>
+                  <ProductCard p={p} />
+                </div>
+              ))}
+            </div>
+          )}
         </div>
       </div>
       <div className="container text-center p-5 ">
